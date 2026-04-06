b89eed535d48e00a6922acd41cc791b0893cf5f5
hast <kvasov.v@routeamgroup.com>
2026-04-06 13:56:58 +0300

    init

--- maxapi_calendar/__init__.py
+++ maxapi_calendar/__init__.py
@@ -1,25 +1,6 @@
-"""
-maxapi_calendar — inline-календарь для ботов мессенджера MAX на базе maxapi.
-
-Порт библиотеки aiogram_calendar (https://github.com/noXplode/aiogram_calendar)
-под Python-библиотеку maxapi (https://love-apples.github.io/maxapi/).
-
-Экспортируемые имена повторяют оригинал, чтобы упростить миграцию:
-
-    from maxapi_calendar import (
-        SimpleCalendar,
-        SimpleCalendarCallback,
-        DialogCalendar,
-        DialogCalendarCallback,
-        get_user_locale,
-    )
-"""
-
 from .simple_calendar import SimpleCalendar
-from .dialog_calendar import DialogCalendar
 from .schemas import (
     SimpleCalendarCallback,
-    DialogCalendarCallback,
     CalendarLabels,
     SimpleCalAct,
     DialogCalAct,
@@ -29,8 +10,6 @@ from .common import get_user_locale
 __all__ = [
     "SimpleCalendar",
     "SimpleCalendarCallback",
-    "DialogCalendar",
-    "DialogCalendarCallback",
     "CalendarLabels",
     "SimpleCalAct",
     "DialogCalAct",
